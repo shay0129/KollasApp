86246a0f58ac7336cfdec633b8c854cfca400759
Shay Mordechai <52373104+shay0129@users.noreply.github.com>
2024-03-25 09:56:53 +0200

    Update and rename main.py to client.py

---
+++ client.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import logging
+from server import display_sub_folders_as_buttons
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    # Specify the root folder ID of the Google Drive folder
+    root_folder_id = '1R03Me1zw4g6r7er3W1xfhldHQAE_v2VP'
+
+    # Create the main window
+    root = tk.Tk()
+    root.geometry("500x500")
+    root.title("Google Drive Folders")
+
+    # Create a frame to hold the buttons
+    frame = tk.Frame(root)
+    frame.pack(padx=10, pady=10)
+
+    # Create a button to view subfolders
+    view_button = tk.Button(frame, text="View Sub-folders", command=lambda: display_sub_folders_as_buttons(root_folder_id, frame))
+    view_button.pack()
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
