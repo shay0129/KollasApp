915fe834f3972fee93a3743a3d9f14bd91aecb49
Shay Mordechai <52373104+shay0129@users.noreply.github.com>
2024-03-25 10:13:39 +0200

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,10 +1,38 @@
+# Request folders and audio files from Google's server, download a temporary file by url, and play it
 from googleapiclient.errors import HttpError
 from utils import transform_google_drive_url
-from google_services import get_service
 from pygame import mixer
-import tkinter as tk
 import logging
 import requests
+
+def search_files(service, query):
+    MAX_RESULTS = 100
+    files = []
+    page_token = None
+    while True:
+        try:
+            response = service.files().list(q=query, spaces='drive',
+                                            fields='nextPageToken, files(id, name, mimeType, webContentLink)',
+                                            pageToken=page_token).execute()
+            files.extend(response.get('files', []))
+            page_token = response.get('nextPageToken', None)
+            if page_token is None or len(files) >= MAX_RESULTS:
+                break
+        except HttpError as error:
+            logging.error(f'An error occurred: {error}')
+            break
+
+    return files
+
+def get_sub_folders_and_files(service, parent_folder_id=None):
+    if parent_folder_id is None:
+        query = "trashed=false"
+    else:
+        query = f"'{parent_folder_id}' in parents and trashed=false"
+
+    folders_and_files = search_files(service, query)
+
+    return folders_and_files
 
 def download_google_drive_audio(url):
     try:
@@ -37,44 +65,6 @@ def play_audio_from_url(url):
     else:
         logging.error("Failed to download audio file")
 
-def display_sub_folders_as_buttons(folder_id, parent_frame):
-    # Obtain the Google Drive service object
-    drive_service = get_service()
-
-    if drive_service:
-        # Fetch subfolders and files of the parent folder
-        sub_folders_and_files = get_sub_folders_and_files(drive_service, folder_id)
-
-        if sub_folders_and_files:
-            # Clear the existing frame
-            for widget in parent_frame.winfo_children():
-                widget.destroy()
-
-            # Create a frame to hold the buttons
-            frame = tk.Frame(parent_frame)
-            frame.pack(padx=10, pady=10)
-
-            # Add buttons for each subfolder or audio file
-            for item in sub_folders_and_files:
-                item_name = item['name']
-                item_id = item['id']
-                button = tk.Button(frame, text=item_name)
-                button.pack(pady=5)
-
-                if item['mimeType'] == 'application/vnd.google-apps.folder':
-                    # Recursively call the function for subfolders
-                    button.config(command=lambda id=item_id, frame=frame: display_sub_folders_as_buttons(id, frame))
-                elif item['mimeType'] == 'audio/mpeg':
-                    audio_url = item['webContentLink']
-                    logging.info(f"Found audio file: {item_name} - Modified URL: {transform_google_drive_url(audio_url)}")
-                    # Play the audio file directly from its URL when the button is pressed
-                    button.config(command=lambda url=audio_url: play_audio_from_url(url))
-
-        else:
-            logging.info("No sub-folders found.")
-    else:
-        logging.error("Failed to obtain Google Drive service.")
-
 def play_audio(url):
     audio_filename = download_google_drive_audio(url)
     if audio_filename:
@@ -89,32 +79,3 @@ def play_audio(url):
             logging.error(f"Error playing audio: {e}")
     else:
         logging.error("Failed to download audio file")
-
-def get_sub_folders_and_files(service, parent_folder_id=None):
-    if parent_folder_id is None:
-        query = "trashed=false"
-    else:
-        query = f"'{parent_folder_id}' in parents and trashed=false"
-
-    folders_and_files = search_files(service, query)
-
-    return folders_and_files
-
-def search_files(service, query):
-    MAX_RESULTS = 100
-    files = []
-    page_token = None
-    while True:
-        try:
-            response = service.files().list(q=query, spaces='drive',
-                                            fields='nextPageToken, files(id, name, mimeType, webContentLink)',
-                                            pageToken=page_token).execute()
-            files.extend(response.get('files', []))
-            page_token = response.get('nextPageToken', None)
-            if page_token is None or len(files) >= MAX_RESULTS:
-                break
-        except HttpError as error:
-            logging.error(f'An error occurred: {error}')
-            break
-
-    return files
